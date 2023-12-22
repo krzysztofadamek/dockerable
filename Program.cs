d81019828220323a1550792555e71f969cec7ae7
krzysztofadamek <krzysztof.adamek@auctane.com>
2023-12-22 10:34:43 +0100

    add project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dockerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.WriteLine("Hello World!");
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
